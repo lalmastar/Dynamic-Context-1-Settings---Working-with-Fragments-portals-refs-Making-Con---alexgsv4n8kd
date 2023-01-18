0395869c2ec4449416e1a14b7a9b30ea40bda1fe
lalmastar <107454522+lalmastar@users.noreply.github.com>
2023-01-18 13:20:40 +0530

    Update Settings.js

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -10,7 +10,7 @@ export const Settings = () => {
     return (
         <div style={{ border: '5px solid red', padding: '8px' }} id="settings">
             <h4>Settings</h4>
-            <input type={'text'} />
+            <input type={'text'} onChange={(e)=>changeGreeting(e.target.value)}/>
         </div>
     )
 }
